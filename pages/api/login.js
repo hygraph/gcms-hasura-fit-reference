8242df328fa661c880416ef69eaa4b1c7e8f8c7a
Jesse <themotleydev@gmail.com>
2020-06-07 14:01:17 +0200

    test x-forwarded header

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,7 +2,11 @@ import auth0 from "../../lib/auth0";
 
 export default async function login(req, res) {
   try {
-    await auth0.handleLogin(req, res);
+    await auth0.handleLogin(req, res, {
+      redirectTo: process.env.VERCEL_URL
+        ? `https://${req["x-forwarded-host"]}`
+        : `http://localhost:3000`,
+    });
   } catch (error) {
     console.error(error);
     res.status(error.status || 500).end(error.message);
