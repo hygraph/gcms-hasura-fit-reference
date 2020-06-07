e7351827e0b374261e2a0b29ccd40e5a57214962
Jesse <themotleydev@gmail.com>
2020-06-07 13:24:50 +0200

    update api routes

--- pages/api/me.js
+++ pages/api/me.js
@@ -1,8 +1,8 @@
 import auth0 from "../../lib/auth0";
 
-export default async function login(req, res) {
+export default async function me(req, res) {
   try {
-    await auth0.handleLogin(req, res);
+    await auth0.handleProfile(req, res);
   } catch (error) {
     console.error(error);
     res.status(error.status || 500).end(error.message);
